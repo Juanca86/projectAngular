0e0206afc16ac8d4f2b8782e437c0679fd722aa8
Charly <plmallorca@gmail.com>
2023-02-15 11:21:12 +0100

    importo moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
+import { ElementsModule } from './elements/elements.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ManagementComponent } from './components/management/management.component';
-import { ModulsComponent } from './moduls/moduls.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ProductsComponent,
-    ManagementComponent,
-    ModulsComponent
+    AppComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
