362c17b803c099433b2ab992b08ddec22ed4d0ad
Charly <plmallorca@gmail.com>
2023-02-18 19:51:25 +0100

    termino formulario

---
+++ src/app/model/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Interfaces {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    stars: number;
+    image: string;
+};
+
+
+export interface gestionFormulario {
+    name: string;
+    price: number;
+    description: string;
+    imgRout: string;
+    opinion: string;
+    };
