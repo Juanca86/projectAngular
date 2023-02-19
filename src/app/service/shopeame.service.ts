ad34b0a8dfabbd5f67c7cb25917427c92a1d7828
i-am-iron-man <iron-man@avengers.com>
2023-02-19 11:36:28 +0100

    actualizo readme

--- src/app/service/shopeame.service.ts
+++ src/app/service/shopeame.service.ts
@@ -9,7 +9,7 @@ export class ShopeameService {
   db_url: string = "http://localhost:3000/products"
 
   constructor(private http: HttpClient) { }
-  
+
   getProducts(){
     return this.http.get(this.db_url)
   }
