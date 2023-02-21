b74cf2b145cb6fff3c4fd16a24b458297dc96193
Charly <plmallorca@gmail.com>
2023-02-21 16:37:21 +0100

    añado boton y buscador

--- src/app/elements/pages/products/products.component.ts
+++ src/app/elements/pages/products/products.component.ts
@@ -10,11 +10,47 @@ import { Items } from 'src/app/model/interfaces';
 export class ProductsComponent {
   productsList: Items[] = [];
 
+  filtrar: Items[] = [];
+
+  userList: boolean = false;
+  cuadricula: boolean = true;
+  mensaje: string = 'Lista';
+  //funcion para el botón
+
+  cambiarVista() {
+    if (this.userList) {
+      this.userList = false;
+      this.cuadricula = true;
+      this.mensaje = 'Lista';
+    } else {
+      this.userList = true;
+      this.cuadricula= false;
+      this.mensaje = 'Cuadricula';
+    }
+    if (this.cuadricula) {
+      this.cuadricula = true;
+      this.userList = false
+      this.mensaje = 'Cuadricula';
+    } else {
+      this.cuadricula = false;
+      this.userList = true;
+      this.mensaje = 'Lista';
+    }
+  }
+
+  //funcion para el buscador
+  webSeeker(filtro: any) {
+    this.filtrar = this.productsList.filter((product) =>
+      product.name.toLowerCase().includes(filtro.toLowerCase())
+    );
+  }
+
   constructor(private shopeameService: ShopeameService) {}
 
   ngOnInit(): void {
     this.shopeameService.getProducts().subscribe((data: any) => {
       this.productsList = [...data];
+      this.filtrar = [...data];
     });
   }
 }
